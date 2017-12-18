415567413f9fc98a66c8a67fcf866cd7b77484fd
Takao Sekiguchi <takao.sekiguchi@soliton.co.jp>
2017-12-18 01:06:52 -0800

    確率的勾配法

---
+++ code/ch02/ch02-04.py
@@ -0,0 +1,48 @@
+from adaline_sgd import *
+from plot_decision_regions import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+# ### Reading-in the Iris data
+import pandas as pd
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/'
+        'machine-learning-databases/iris/iris.data', header=None)
+df.tail()
+
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+
+### 正規化してから勾配法つかう
+# standardize features
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+
+### 確率的勾配法
+ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Stochastic Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+#plt.savefig('./adaline_4.png', dpi=300)
+plt.show()
+
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Average Cost')
+
+plt.tight_layout()
+# plt.savefig('./adaline_5.png', dpi=300)
+plt.show()
