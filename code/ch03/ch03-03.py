155a916dc98f8f88717ebe82bdb7b16672553dda
Takao Sekiguchi <takao.sekiguchi@soliton.co.jp>
2017-12-28 02:03:31 -0800

    logistic regression: regularization

---
+++ code/ch03/ch03-03.py
@@ -0,0 +1,68 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Perceptron
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.tree import export_graphviz
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import warnings
+from plot_decision_regions import *
+
+# for sklearn 0.18's alternative syntax
+from distutils.version import LooseVersion as Version
+from sklearn import __version__ as sklearn_version
+if Version(sklearn_version) < '0.18':
+    from sklearn.grid_search import train_test_split
+else:
+    from sklearn.model_selection import train_test_split
+
+#############################################################################
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+print('Class labels:', np.unique(y))
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=0)
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+#############################################################################
+print(50 * '=')
+print('Section: Tackling overfitting via regularization')
+print(50 * '-')
+
+weights, params = [], []
+for c in np.arange(-5.0, 5.0):
+    lr = LogisticRegression(C=10**c, random_state=0)
+    lr.fit(X_train_std, y_train)
+    # print(lr.coef_.shape)    # 3x2
+    weights.append(lr.coef_[1])
+    # weights.append(lr.coef_[2])
+    # print(lr.coef_)
+    # print(lr.intercept_)
+    params.append(10**c)
+
+weights = np.array(weights)
+plt.plot(params, weights[:, 0],
+         label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--',
+         label='petal width')
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+plt.xscale('log')
+# plt.savefig('./figures/regression_path.png', dpi=300)
+plt.show()
