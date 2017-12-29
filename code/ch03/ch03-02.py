763baa6e00ec7d13236926dec02d07155112f978
Takao Sekiguchi <takao.sekiguchi@soliton.co.jp>
2017-12-29 00:14:50 -0800

    確率の値の検算, 少し合わない?

--- code/ch03/ch03-02.py
+++ code/ch03/ch03-02.py
@@ -46,6 +46,7 @@ print(50 * '-')
 
 
 lr = LogisticRegression(C=1000.0, random_state=0)
+#lr = LogisticRegression(C=0.01, random_state=0)
 lr.fit(X_train_std, y_train)
 
 plot_decision_regions(X_combined_std, y_combined,
@@ -59,3 +60,23 @@ plt.show()
 
 print('Predicted probabilities', lr.predict_proba(X_test_std[0, :]
                                                   .reshape(1, -1)))
+
+# seki: confirms that probability is equal to sigmoid(w^t x)
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+print('coef_', lr.coef_)
+print('intercept_', lr.intercept_)
+print('X_test_std[0]', X_test_std[0, :])
+# [[-7.34015187 -6.64685581]
+#   [ 2.54373335 -2.3421979 ]
+#   [ 9.46617627  6.44380858]]
+# [-9.31757401 -0.89462847 -8.85765974]
+# [ 0.70793846  1.50872803]
+
+w = np.c_[lr.intercept_.reshape(3,1), lr.coef_]
+print('W', w)
+x = np.r_[np.array([1]), X_test_std[0, :]]
+print('x', x)
+print('W^t x', np.dot(w, x))
+print('sigmoid(W^t, x)', sigmoid(np.dot(w, x)))
