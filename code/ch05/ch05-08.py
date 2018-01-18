5b394d111847a3ae74f1b5bf52e12a553b450e1d
Takao Sekiguchi <takao.sekiguchi@soliton.co.jp>
2018-01-18 01:53:41 -0800

    サブルーチン移動

--- code/ch05/ch05-08.py
+++ code/ch05/ch05-08.py
@@ -13,6 +13,7 @@ from scipy.spatial.distance import pdist, squareform
 from scipy import exp
 from scipy.linalg import eigh
 from matplotlib.ticker import FormatStrFormatter
+from rbf_kernel_pca import *
 
 # for sklearn 0.18's alternative syntax
 from distutils.version import LooseVersion as Version
@@ -29,59 +30,6 @@ print(50 * '=')
 print('Section: Projecting new data points')
 print(50 * '-')
 
-
-def rbf_kernel_pca(X, gamma, n_components):
-    """
-    RBF kernel PCA implementation.
-
-    Parameters
-    ------------
-    X: {NumPy ndarray}, shape = [n_samples, n_features]
-
-    gamma: float
-      Tuning parameter of the RBF kernel
-
-    n_components: int
-      Number of principal components to return
-
-    Returns
-    ------------
-     X_pc: {NumPy ndarray}, shape = [n_samples, k_features]
-       Projected dataset
-
-     lambdas: list
-       Eigenvalues
-
-    """
-    # Calculate pairwise squared Euclidean distances
-    # in the MxN dimensional dataset.
-    sq_dists = pdist(X, 'sqeuclidean')
-
-    # Convert pairwise distances into a square matrix.
-    mat_sq_dists = squareform(sq_dists)
-
-    # Compute the symmetric kernel matrix.
-    K = exp(-gamma * mat_sq_dists)
-
-    # Center the kernel matrix.
-    N = K.shape[0]
-    one_n = np.ones((N, N)) / N
-    K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
-
-    # Obtaining eigenpairs from the centered kernel matrix
-    # numpy.eigh returns them in sorted order
-    eigvals, eigvecs = eigh(K)
-
-    # Collect the top k eigenvectors (projected samples)
-    alphas = np.column_stack((eigvecs[:, -i]
-                              for i in range(1, n_components + 1)))
-
-    # Collect the corresponding eigenvalues
-    lambdas = [eigvals[-i] for i in range(1, n_components + 1)]
-
-    return alphas, lambdas
-
-
 X, y = make_moons(n_samples=100, random_state=123)
 
 plt.figure(1)
@@ -89,7 +37,7 @@ plt.scatter(X[y == 0, 0], X[y == 0, 1], color='red', marker='^', alpha=0.5)
 plt.scatter(X[y == 1, 0], X[y == 1, 1], color='blue', marker='o', alpha=0.5)
 #plt.show()
 
-alphas, lambdas = rbf_kernel_pca(X, gamma=15, n_components=1)
+alphas, lambdas = rbf_kernel_pca2(X, gamma=15, n_components=1)
 
 
 x_new = X[25]
--- code/ch05/rbf_kernel_pca.py
+++ code/ch05/rbf_kernel_pca.py
@@ -58,3 +58,60 @@ def rbf_kernel_pca(X, gamma, n_components):
     # よって sqrt(lambda_i) a_i で主成分方向に基底変換したデータサンプルになる。
 
     return X_pc
+
+
+##
+# 本文の後で出てくるバージョン
+# 計算は一緒で返すものが違うだけ
+# 固有値と固有ベクトルを返す
+# 
+def rbf_kernel_pca2(X, gamma, n_components):
+    """
+    RBF kernel PCA implementation.
+
+    Parameters
+    ------------
+    X: {NumPy ndarray}, shape = [n_samples, n_features]
+
+    gamma: float
+      Tuning parameter of the RBF kernel
+
+    n_components: int
+      Number of principal components to return
+
+    Returns
+    ------------
+     X_pc: {NumPy ndarray}, shape = [n_samples, k_features]
+       Projected dataset
+
+     lambdas: list
+       Eigenvalues
+
+    """
+    # Calculate pairwise squared Euclidean distances
+    # in the MxN dimensional dataset.
+    sq_dists = pdist(X, 'sqeuclidean')
+
+    # Convert pairwise distances into a square matrix.
+    mat_sq_dists = squareform(sq_dists)
+
+    # Compute the symmetric kernel matrix.
+    K = exp(-gamma * mat_sq_dists)
+
+    # Center the kernel matrix.
+    N = K.shape[0]
+    one_n = np.ones((N, N)) / N
+    K = K - one_n.dot(K) - K.dot(one_n) + one_n.dot(K).dot(one_n)
+
+    # Obtaining eigenpairs from the centered kernel matrix
+    # numpy.eigh returns them in sorted order
+    eigvals, eigvecs = eigh(K)
+
+    # Collect the top k eigenvectors (projected samples)
+    alphas = np.column_stack((eigvecs[:, -i]
+                              for i in range(1, n_components + 1)))
+
+    # Collect the corresponding eigenvalues
+    lambdas = [eigvals[-i] for i in range(1, n_components + 1)]
+
+    return alphas, lambdas
